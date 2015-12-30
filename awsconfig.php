db67829e3ab9da30b0cab79d9c6110f43ff4149b
m161998 <matthewgfinley@googlemail.com>
2015-12-29 19:36:54 -0500

    added some server codes

---
+++ awsconfig.php
@@ -0,0 +1,11 @@
+<?php
+	//return keys necessary for SES and S3 uploads
+	//Keys for Moore. Store file in /var/www/
+	return [
+		's3' => [
+			'key' => 'AKIAIWTPVMQGXAC2FKNA',
+			'secret' => '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',
+			'bucket' => 'midtrade'
+		]
+	];
+?>
---
+++ con.php
@@ -0,0 +1,3 @@
+<?php
+	$oopcon = new mysqli("development.c4cfejmills6.us-east-1.rds.amazonaws.com","devGroup","devGroup","MidTrade");
+?>
