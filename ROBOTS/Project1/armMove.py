3465f70f6c3e5f20cdebcce03ba43a383e68cdd8
Finley <matthewgfinley@googlemail.com>
2016-02-02 15:18:56 -0500

    more robots

--- ROBOTS/Project1/armMove.py
+++ ROBOTS/Project1/armMove.py
@@ -21,7 +21,7 @@ def Amatrix(l,d,alpha,theta):
 			]
 	temp = np.dot(matrix1,matrix2)
 	temp = np.dot(temp,matrix3)
-	print(temp)
+	#print(temp)
 	return temp
 	#print(temp[0][3],temp[1][3],temp[2][3])
 
@@ -30,36 +30,62 @@ def numConvert(numberTicks):
 
 	return temp
 
+def radiansToTicks(rads):
+	angle = np.round(rads/np.pi*255)
+	return(angle)
+
+
 #print(Amatrix(9.5,0,0,0))
-angles = [63,230,5,20,128]
-#angles = [128,128,128,128,128]
+angles = [128,158,45,100,190] #SAME AS PICTURE
+
+#angles2 = [0,-.3665,1.029744,.34906,-.76794]
+angles2 = [0,0,0,-np.pi/4,0]
 print(numConvert(angles[0]))
 print(numConvert(angles[1]))
-#1
-position = Amatrix(10,0,0,numConvert(angles[0]))
-print(position[0][3],position[1][3],position[2][3])
-#2
-position = np.dot(position , Amatrix(9,0, 90, numConvert(angles[1])))
-print(position[0][3],position[1][3],position[2][3])
-#3
-position = np.dot(position , Amatrix(4,0, 0, numConvert(angles[2])))
-print(position[0][3],position[1][3],position[2][3])
-#4
-position = np.dot(position , Amatrix(0,0, np.pi/2, numConvert(angles[3]) - np.pi/2))
-print(position[0][3],position[1][3],position[2][3])
-#5
-position = np.dot(position , Amatrix(0,7.5, 0, numConvert(angles[4])))
-print(position[0][3],position[1][3],position[2][3])
+print(numConvert(angles[2]))
+print(numConvert(angles[3]))
 print(numConvert(angles[4]))
+
+#1
+position = Amatrix(10,0,0,angles2[0])
+print(position)
+print()
+#2
+position = np.dot(position , Amatrix(9,0, np.pi/2, angles2[1]))
+print(position)
+print()
+#3
+position = np.dot(position , Amatrix(4,0, 0, angles2[2]))
+print(position)
+print()
+#4
+position = np.dot(position , Amatrix(0,0, -np.pi/2, angles2[3] - np.pi/2 ))
+print(position)
+print()
+#5
+position = np.dot(position , Amatrix(0,7.5, 0, angles2[4] ))
+print(position)
+
+
+angle1 =  127 - radiansToTicks(angles2[0]) 
+angle2 =  127 - radiansToTicks(angles2[1])
+angle3 =  127 - radiansToTicks(angles2[2])
+angle4 =  127 + radiansToTicks(angles2[3])
+angle5 =  127 - radiansToTicks(angles2[4])
+
+print(angle1)
+print(angle2)
+print(angle3)
+print(angle4)
+print(angle5)
 
 
 ser = serial.Serial("/dev/ttyUSB0") #Connects to the USB port
-temp = numConvert(0)
-command1 = "SV1 M" + str(angles[0]) + "\r"
-command2 = "SV2 M" + str(angles[1]) + "\r"
-command3 = "SV3 M" + str(angles[2]) + "\r"
-command4 = "SV4 M" + str(angles[3]) + "\r"
-command5 = "SV5 M" + str(angles[4]) + "\r"
+command1 = "SV1 M" + str(angle1) + "\r"
+command2 = "SV2 M" + str(angle2) + "\r"
+command3 = "SV3 M" + str(angle3) + "\r"
+command4 = "SV4 M" +  str(angle4) + "\r"
+command5 = "SV5 M" + str(angle5) + "\r"
 
 ser.write(command1) #First command "wakes up" the controller board
 ser.write(command2)
