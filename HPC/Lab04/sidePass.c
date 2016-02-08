e0336021468279e4b8893b6538f0c4c9258bafdf
Finley <matthewgfinley@googlemail.com>
2016-02-08 15:21:23 -0500

    sdt upload

--- HPC/Lab04/sidePass.c
+++ HPC/Lab04/sidePass.c
@@ -5,12 +5,12 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[] ){
-	
+	int clockwise = true;
 	int rank;
 	int numprocs;
 	int number;
 	int xx;
-	
+	int nextNode;
 	
 	MPI_Init(&argc,&argv);
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
@@ -30,13 +30,41 @@ int main(int argc, char *argv[] ){
 	}
 
 	while(1){
-
 		MPI_Recv(&number, 1, MPI_INT, MPI_ANY_SOURCE , MPI_ANY_TAG , MPI_COMM_WORLD, &stat);
-		printf(" I have recieved a message from rank %d Passing now\n", stat.MPI_SOURCE);
-		if(stat.MPI_SOURCE > stat.MPI_TAG){
+		if(clockwise == true){
+			if( rank + 1 == numprocs){
+				nextNode = 0;
+			}
+			else{
+				nextNode = rank+1;
+			}
+
+
+			printf("Node: %d, final: %d, Next: %d")
+			if(rank < stat.MPI_TAG){
 			MPI_Send(&number, 1, MPI_INT, rank+1, stat.MPI_TAG, MPI_COMM_WORLD);
 		}
+			if(stat.MPI_SOURCE == stat.MPI_TAG){
+			printf("message made it to destination");
+		}
+
+
+
+
+		}
+
+
+
+
+
+
+
+		MPI_Recv(&number, 1, MPI_INT, MPI_ANY_SOURCE , MPI_ANY_TAG , MPI_COMM_WORLD, &stat);
+		printf("%d: recieved a message from rank %d Final destination of %d passing to %d now\n",rank, stat.MPI_SOURCE, stat.MPI_TAG, 5);
 		if(stat.MPI_SOURCE < stat.MPI_TAG){
+			MPI_Send(&number, 1, MPI_INT, rank+1, stat.MPI_TAG, MPI_COMM_WORLD);
+		}
+		if(stat.MPI_SOURCE > stat.MPI_TAG){
 			MPI_Send(&number, 1, MPI_INT, rank-1, stat.MPI_TAG, MPI_COMM_WORLD);
 		}
 		if(stat.MPI_SOURCE == stat.MPI_TAG){
