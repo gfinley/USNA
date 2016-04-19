9b1998226408c4d4f1b388435185f703fc0be211
m161998 <matthewgfinley@googlemail.com>
2016-04-19 14:59:55 -0400

    Started on Wumpus hunter

---
+++ ROBOTS/FinalProject/WumpusHunt.py
@@ -0,0 +1,8 @@
+#this is for the wompus hunt
+#functions are goint to include
+
+def getColorInRoom():
+    #this function will identify the color seen in the room if any
+    #probably utilize opencv
+    klm
+    
